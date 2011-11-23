db974a3dc92a337192751b5763733c5e8e351157
Jon Parise <jon@php.net>
2011-11-23 13:05:39 -0500

    Move SMTP.php into a Net subdirectory.
    This should have been done a lone time ago.  This change lets tests
    run from the source directory (using 'Net/SMTP.php') without resorting
    to symlink hacks.

--- phpdoc.sh
+++ phpdoc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-phpdoc -f SMTP.php -t docs/api -p -ti "Net_SMTP Package API" -dn Net_SMTP -dc Net_SMTP -ed examples -i CVS/*
+phpdoc -f Net/SMTP.php -t docs/api -p -ti "Net_SMTP Package API" -dn Net_SMTP -dc Net_SMTP -ed examples
